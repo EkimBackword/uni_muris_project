9c2a4065869680fed6bd470f3f7554bd62eaa1ed
MikhailZharkov <zharkov@abcreg.ru>
2018-03-06 22:52:07 +0300

    redesign

--- frontend/src/app/appRouting.routing.ts
+++ frontend/src/app/appRouting.routing.ts
@@ -7,13 +7,22 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LessonComponent } from './lesson/lesson.component';
 import { AdminPageComponent } from './main/admin-page/admin-page.component';
 import { UniPageComponent } from './main/uni-page/uni-page.component';
+import { AdminSubjectComponent } from './main/admin-page/subject/subject.component';
+import { AdminUserComponent } from './main/admin-page/user/user.component';
+import { AdminGroupComponent } from './main/admin-page/group/group.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'main',
     component: MainComponent,
     children: [
-      { path: 'admin', component: AdminPageComponent },
+      { path: 'admin', component: AdminPageComponent,
+        children: [
+          { path: 'group', component: AdminGroupComponent},
+          { path: 'subject', component: AdminSubjectComponent},
+          { path: 'user', component: AdminUserComponent}
+        ]
+      },
       { path: 'uni', component: UniPageComponent },
     ]
   },
